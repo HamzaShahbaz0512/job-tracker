ef4b44c9c84b0c18e26dd310c2fd231685a9bfb8
Hamza Shahbaz <121577494+HamzaShahbaz0512@users.noreply.github.com>
2026-07-10 01:08:15 +0500

    All the endpoints are ready

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from flask_jwt_extended import JWTManager
 from flask_jwt_extended import create_access_token
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import jwt_required
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("JWT_SECRET_KEY")
+
 
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///job.db"
@@ -30,10 +36,12 @@ class Job(db.Model):
 with app.app_context():
     db.create_all()
 
-app.config['JWT_SECRET_KEY']='1'
+app.config['JWT_SECRET_KEY']=api_key
 JWT=JWTManager(app)
 print("DB created successfully")
 
+
+#Signup the user up
 @app.route("/signup",methods=["POST"])
 def signup():
     data=request.get_json()
@@ -42,11 +50,143 @@ def signup():
     password=data.get("password")
     hashed_password=generate_password_hash(password)
     
-    if(Users.query.filter_by(name=name1).first()):
-        return jsonify({'error':'User with this name already exists'}),400
+    if(Users.query.filter_by(email=email).first()):
+        return jsonify({'error':'User with this email already exists'}),400
     
     new_user=Users(name=name1,email=email,password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
     
     return jsonify({"message": f'User with {email} created'}), 201
+
+
+@app.route("/login",methods=["POST"])
+def login():
+    data = request.get_json()
+    email=data.get("email")
+    password=data.get("password")
+    
+    user=Users.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password,password):
+        return jsonify({"error":"Invalid email or password"}),401
+    
+    access_token=create_access_token(identity=user.email)
+    return jsonify({"Logged in Successfully ":access_token}),200
+    
+
+#create a new job
+@app.route("/create_job",methods=["POST"])
+@jwt_required()
+def create_job():
+    data=request.get_json()
+    job_id=data.get("job_id")
+    company=data.get("company")
+    description=data.get("description")
+    position=data.get("position")
+    status=data.get("status")
+    user_id=data.get("user_id")
+    date_applied=data.get("date_applied")
+
+    if job_id is None or company is None or description is None or position is None or status is None or user_id is None or date_applied is None:
+        return jsonify({"error": "Missing required fields"}), 400
+    
+    new_job=Job(
+        job_id=job_id,
+        company=company,
+        description=description,
+        position=position,
+        status=status,
+        user_id=user_id,
+        date_applied=date_applied
+    )
+    db.session.add(new_job)
+    db.session.commit()
+    return jsonify({"message": f'Job with {job_id} created'}), 201
+
+#return all jobs    
+@app.route("/get_jobs",methods=["GET"])
+@jwt_required()
+def get_jobs():
+    jobs=Job.query.all()
+    job_list=[]
+    if(jobs is None):
+        return jsonify({"message":"No jobs found"}),404
+    
+    for job in jobs:
+        job_data={
+            "job_id":job.job_id,
+            "company":job.company,
+            "description":job.description,
+            "position":job.position,
+            "status":job.status,
+            "user_id":job.user_id,
+            "date_applied":job.date_applied
+        }
+        job_list.append(job_data)
+    return jsonify(job_list)
+
+
+#get a specific job
+@app.route("/get_job/<job_id>",methods=["GET"])
+@jwt_required()
+def get_job(job_id):
+    job=Job.query.filter_by(job_id=job_id).first()
+    if job is None:
+        return jsonify({"message":"Job not found"}),404
+    return jsonify({
+        "job_id":job.job_id,
+        "company":job.company,
+        "description":job.description,
+        "position":job.position,
+        "status":job.status,
+        "user_id":job.user_id,
+        "date_applied":job.date_applied
+    })
+    
+#Jobs assigned to specific user
+@app.route("/myJobs/<user_id>",methods=["GET"])
+@jwt_required()
+def my_jobs(user_id):
+    print("Hello")
+    jobs=Job.query.filter_by(user_id=user_id).all()
+    
+    if(jobs is None):
+        return jsonify({"message":"No jobs found"}),404
+
+    return jsonify([{
+        "job_id":job.job_id,
+        "company":job.company,
+        "description":job.description,
+        "position":job.position,
+        "status":job.status,
+        "user_id":job.user_id,
+        "date_applied":job.date_applied
+    } 
+        for job in jobs]) 
+    
+#update job
+@app.route("/update_job/<job_id>",methods=["PUT"])
+@jwt_required()
+def update_job(job_id):
+    data=request.get_json()
+    job=Job.query.get(id)
+    
+    job=Job.query.get(job_id)
+    if not job:
+        return jsonify({"message":"Job not found"}),404
+    if(data.get("company")):
+        job.company=data.get("company")
+    if(data.get("description")):
+        job.description=data.get("description")
+    if(data.get("position")):
+        job.position=data.get("position")
+    if(data.get("status")):
+        job.status=data.get("status")
+    if(data.get("application_date")):
+        job.date_applied=data.get("date_applied")
+    db.session.commit()
+    return jsonify({"message":"Job updated successfully"}),200
+        
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
